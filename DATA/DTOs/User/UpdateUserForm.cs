ddd99ba4fb5c2db6d24f38438dcf7ef5bbe5fd98
Ash <husseinfahmi1999@gmail.com>
2024-10-29 20:35:58 +0300

    sss

--- DATA/DTOs/User/UpdateUserForm.cs
+++ DATA/DTOs/User/UpdateUserForm.cs
@@ -9,10 +9,10 @@ namespace EvaluationBackend.DATA.DTOs.User
         public string? UserName { get; set; }
         public string? FullName { get; set; }
         public string? PhoneNumber { get; set; }
-        public DateTime BirthDay { get; set; }
+        public string? Password { get; set; }
+
         public int? Role { get; set; }
         public string? Img { get; set; }
 
-        // public string? Password { get; set; }
     }
 }
